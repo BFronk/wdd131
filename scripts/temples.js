0c02240875ef9eef5c1af7d53cfb82a102db18bb
Bridger Fronk <bridgerfronk@gmail.com>
2024-07-11 20:41:09 -0400

    update to site

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,3 +1,14 @@
+
+// Update the current year
+const currentYearElement = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+currentYearElement.textContent = currentYear;
+
+// Update the last modified date
+const lastModifiedElement = document.getElementById('lastModified');
+const lastModifiedDate = new Date(document.lastModified);
+lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navLinks');
 

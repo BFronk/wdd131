c0da46d0fa56e13dec77a6da6ed180dd87985ae3
Bridger Fronk <bridgerfronk@gmail.com>
2024-07-26 02:13:03 -0400

    updated project

--- project/scripts/iphone-history.js
+++ project/scripts/iphone-history.js
@@ -1,45 +1,18 @@
+const menubar = document.querySelector('#menu');
+const header_link = document.querySelector('.header_links');
+const menubar2 = document.querySelector('.navigation');
 const pageTitle = document.querySelector('.title');
 const homeButton = document.querySelector('.home');
 const iosButton = document.querySelector('.ios');
 const authButton = document.querySelector('.auth');
 const graphButton = document.querySelector('.graphs');
 const featuresButton = document.querySelector('.features');
+const header = document.querySelector('header');
 
-// function createLayout() {
-   
-// }
-// homeButton.addEventListener('click', () => {
-//     document.querySelector('.active').classList.toggle('active');
-//     pageTitle.textContent = 'History of the iPhone';
-//     homeButton.classList.toggle('active');
-// });
-// iosButton.addEventListener('click', () => {
-//     document.querySelector('.active').classList.toggle('active');
-//     pageTitle.textContent = 'IOS';
-//     homeButton.classList.toggle('active');
-// });
 
-// iosButton.addEventListener('click', () => {
-// 	document.querySelector('.active').classList.toggle('.active');
-// 	pageTitle.textContent = 'Old';
-// 	iosButton.classList.toggle('.active');
-// })
-
-homeButton.addEventListener('click', () => {
-    document.querySelector('.active').classList.toggle('active');
-    pageTitle.textContent = 'home';
-    homeButton.classList.toggle('active');
+menubar.addEventListener('click', () => {
+    menubar.classList.toggle('show');
+    menubar2.classList.toggle('show');
+    header_link.classList.toggle('show');
+    header.classList.toggle('show')
 });
-
-iosButton.addEventListener('click', () => {
-    document.querySelector('.active').classList.toggle('active');
-    pageTitle.textContent = 'iOS';
-    iosButton.classList.toggle('active');
-});
-
-authButton.addEventListener('click', () => {
-    document.querySelector('.active').classList.toggle('active');
-    pageTitle.textContent = 'auth';
-    authButton.classList.toggle('active');
-});
-

9da984cdb69301755ea199e018c6b1728ea203a6
Bridger Fronk <bridgerfronk@gmail.com>
2024-12-19 23:59:02 -0500

    update

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -7,3 +7,16 @@ currentYearElement.textContent = currentYear;
 const lastModifiedElement = document.getElementById('lastModified');
 const lastModifiedDate = new Date(document.lastModified);
 lastModifiedElement.textContent = `Last Modification: ${lastModifiedDate.toLocaleDateString()} ${lastModifiedDate.toLocaleTimeString()}`;
+
+const mainTitle = document.querySelector("#main-title");
+const mainNav = document.querySelector("#main-navi");
+const mainLink = document.querySelector("#main-link");
+const gamesLink = document.querySelector("#games-link");
+const formLink = document.querySelector("#form-link");
+
+mainTitle.addEventListener('click', () => {
+	mainNav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+})
+
+
